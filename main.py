621d2b122b3083e6bc58aae159b8273ac60f9790
Serzhanov Nurbek <68291178+Serzhanov@users.noreply.github.com>
2021-11-26 01:43:32 +0300

    Optimized the code
    Optimized the code, got the right labels of loss value and metrics value of the model.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import numpy as np
 import os
 from tkinter import *
 import classifier
 from PIL import Image
 import network
+global vars
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 class Paint(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
-        self.Cheking_btn_apply=False
+        self.Checking_btn_apply=False
         self.parent = parent
         self.color = "black"
-        self.brush_size = 5
+        self.brush_size = 6
         self.setUI()
         self.button_Reset = Button(
             text="Reset!",
@@ -27,16 +27,20 @@ class Paint(Frame):
             height=1,
             bg="white",
             fg="black",
-            command=lambda: [self.calculating(),self.which_button(), self.b_clicked(), self.save()],
+            command=lambda: [self.save_image(),self.calculating(), self.b_clicked()],
             font=25
         )
         self.button_Reset.pack(side=TOP)
         self.button_Apply.pack(side=TOP)
+        self.model_loss_metrics_values = Label(
+            root, textvariable="", relief=RAISED)
+        self.resultat_output = Label(
+            root, textvariable="", relief=RAISED)
     def draw(self, event):
-        self.canv.create_rectangle(event.x - 5, event.y - 5, event.x + 5, event.y + 5,
+        self.canv.create_rectangle(event.x - 6, event.y -6 , event.x + 6, event.y + 6,
                               fill=self.color, outline=self.color)
     def setUI(self):
-        self.canv = Canvas(self,height=200,width=300)
+        self.canv = Canvas(self,height=375,width=475)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.canv.grid(padx=5, pady=5, sticky=E + W + S + N)
@@ -47,34 +51,32 @@ class Paint(Frame):
         self.setUI()
         if self.resultat_output.winfo_ismapped()==1:
             self.resultat_output.forget()
-    def save(self):
+    def save_image(self):
         self.canv.update()
-        self.canv.postscript(file="test.xps", colormode="color")
-        im2 = Image.open("test.xps")
-        im2.save("test.png", 'png')
+        self.canv.postscript(file="Images\\test.xps", colormode="color")
+        im2 = Image.open("Images\\test.xps")
+        im2.save("Images\\test.png", 'png')
     def check(self):
         if self.button_Apply['state'] == DISABLED:
             self.button_Apply['state'] = ACTIVE
-
     def b_clicked(self):
-        self.Cheking_btn_apply = not self.Cheking_btn_apply
-        self.button_Apply['state']=DISABLED
-        self.resultat_output.pack()
-
-    def which_button(self):  # res
-        self.button_Apply['state'] = ACTIVE
+        if self.Checking_btn_apply == True:
+            self.button_Apply['state']=ACTIVE
+            self.Checking_btn_apply=False
+        else:
+            self.Checking_btn_apply = not self.Checking_btn_apply
+            self.button_Apply['state']=DISABLED
     def calculating(self):
         im=classifier.get_image("Images\\test.png")
         res=network.modeling(im)
-        vars = StringVar()
-        vars.set("The result is " + str(res[0]) + "\n loss is " + str(res[1][0])+"accuracy is" + str(res[1][1]))
-        self.resultat_output = Label(
-            root, textvariable=vars,relief=RAISED)
-
+        print((network.val_loss*100,network.val_acc*100))
+        self.model_loss_metrics_values.config(text=f'Loss value of model with test data is %d percentages and Metrics value for the model with test data is %d percentages\n\n' %(network.val_loss*100,network.val_acc*100))
+        self.model_loss_metrics_values.pack()
+        self.resultat_output.config(text=f'The result of image evaluation is %d\n'% res)
+        self.resultat_output.pack()
 
 WIDTH,HEIGHT=500,600
 root = Tk()
-
 fr=Frame(root)
 fr.pack()
 root.geometry("500x600")
