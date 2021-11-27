dfbfa74b7e78517474cf3abd7b11cc10f067280d
Nurbek <ns490367@gmail.com>
2021-11-27 03:30:15 +0300

    Added some visualization of analysis.

--- network.py
+++ network.py
@@ -4,15 +4,16 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
+from keras.callbacks import History
 check=False
 val_acc=0
 val_loss=0
 res=0
+hist=History()
 def apprentisage():
-    global val_acc,val_loss
+    global val_acc,val_loss,hist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train / 255.0
     x_test = x_test / 255.0
@@ -24,13 +25,13 @@ def apprentisage():
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(units=128,input_shape=(784,),activation='relu'),
         Dense(128, activation='relu'),
-        Dropout(0.25),
         Dense(10, activation='softmax')
     ])
     model.compile(optimizer='adam',  # optimizer
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    model.fit(x_train, y_train_cat, batch_size=32, epochs=7)
+    hist=model.fit(x_train, y_train_cat, batch_size=32, epochs=7,validation_data=(x_train,y_train_cat))
+    plt.plot(hist.history['loss'])
     val_loss, val_acc = model.evaluate(x_test, y_test_cat)  # evaluate the out of sample data with model
     return model
 
@@ -50,7 +51,17 @@ def checking_existing_of_model():
         check=True
 
 def analysis():
-    global model
+    global model,hist
+    #Plot loss and val_loss
+
+    plt.plot(hist.history['loss'],label="loss")
+    plt.plot(hist.history['val_loss'],label="val_loss")
+    plt.plot(hist.history['accuracy'],label="accuracy")
+    plt.plot(hist.history['val_accuracy'],label="val_accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Validation  in %")
+    plt.title("Graphic of fit")
+
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     y_test = keras.utils.to_categorical(y_test, 10)#categorization for our matrix of confusin
     x_test = x_test.reshape(x_test.shape[0], -1)#reshape to matrice with 784 elemetns because in the begining Mnist gives us 28x28
@@ -59,12 +70,13 @@ def analysis():
     y_true=np.argmax(y_test,axis=1)
     matrix=confusion_matrix(y_true,y_predicted_classes)
     #Plot
+    plt.figure(1)
     fig, ax = plt.subplots(figsize=(10, 5))
     ax = sns.heatmap(matrix, annot=True, fmt='d', ax=ax, cmap="Blues")
     ax.set_xlabel('Predicted Label')
     ax.set_ylabel('True Label')
     ax.set_title('Confusion Matrix');
-    plt.show()
+    plt.title("Confusion Matrix")
 
     #Errors
     errors=(y_predicted_classes-y_true !=0)
@@ -82,7 +94,10 @@ def analysis():
 
     # Show Top Errors
     num = len(top_idx_diff_errors)
+    plt.figure(2)
+
     f, ax = plt.subplots(1, num, figsize=(30, 30))
+    f.suptitle("Last 5 errors on the validation data")
 
     for i in range(0, num):
         idx = top_idx_diff_errors[i]
@@ -91,4 +106,6 @@ def analysis():
         y_p = y_pred_classes_errors[idx]
         ax[i].imshow(sample, cmap='gray')
         ax[i].set_title("Predicted label :{}\nTrue label: {}".format(y_p, y_t), fontsize=12)
+
     plt.show()
+
